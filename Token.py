3cc9110a7df16c7e05ec827310e4494f91dbddbb
Matalya <matalya8@gmail.com>
2025-04-05 16:58:11 -0300

    Initial commit - Thorn lexer and token system

---
+++ Token.py
@@ -0,0 +1,121 @@
+from enum import Enum, auto
+
+class TokenKind(Enum):
+    # Internal / meta
+    DEBUG_NULL    = auto() #✅
+    DEBUG_UNKNOWN = auto() #✅
+    EOF_KIND      = auto() #✅
+
+    # Literals
+    INTEGER       = auto() #✅
+    DECIMAL       = auto() #✅
+    STRING        = auto() #✅
+    COMPOSITE_STR = auto() #✅
+
+    # Identifiers / keywords
+    IDENTIFIER    = auto() #✅
+    INT           = auto() #✅
+    STR           = auto() #✅
+    FLOAT         = auto() #✅
+    BOOL          = auto() #✅
+    CHAR          = auto() #✅
+    ANY           = auto() #✅
+    NIL           = auto() #✅
+
+    # Flow control
+    IF            = auto() #❌
+    ELSIF         = auto() #❌
+    ELSE          = auto() #❌
+    WHILE         = auto() #❌
+    UNTIL         = auto() #❌
+    FOR           = auto() #❌
+    FOREACH       = auto() #❌
+    
+    # Symbols
+    ASSIGN        = auto() #✅
+    DOT           = auto() #✅
+    QUESTION_MARK = auto() #✅
+
+    # Arithmetic operators
+    PLUS          = auto() #✅
+    MINUS         = auto() #✅
+    ASTERISK      = auto() #✅
+    SLASH         = auto() #✅
+    EXPONENT      = auto() #✅ ✅
+    MODULO        = auto() #✅
+    DOUBLE_SLASH  = auto() #✅
+
+    # Logical comparison
+    EQUALS        = auto() #✅
+    NOT_EQUAL     = auto() #✅ ✅
+    GREATER_THAN  = auto() #✅
+    LESS_THAN     = auto() #✅
+    GREATER_EQUAL = auto() #✅ ✅
+    LESS_EQUAL    = auto() #✅ ✅
+    # Logical operators
+    AND           = auto() #✅
+    OR            = auto() #✅
+    XOR           = auto() #✅
+    NOR           = auto() #✅
+    NAND          = auto() #✅
+    # Logical literals
+    TRUE          = auto() #✅
+    FALSE         = auto() #✅
+
+    # Delimeters
+    OPEN_PAREN    = auto() #✅
+    CLOSE_PAREN   = auto() #✅
+    OPEN_CURLY    = auto() #✅
+    CLOSE_CURLY   = auto() #✅
+    OPEN_BRACK    = auto() #✅
+    CLOSE_BRACK   = auto() #✅
+    SEMICOLON     = auto() #✅
+
+    def Name(self):
+        return self.name.lower()
+
+def symbolize(tokenKind):
+    match tokenKind:
+        case TokenKind.PLUS:
+            return "+"
+        case TokenKind.MINUS:
+            return "-"
+        case TokenKind.ASTERISK:
+            return "*"
+        case TokenKind.SLASH:
+            return "/"
+        case TokenKind.EOF_KIND:
+            return "\0"
+        case TokenKind.ASSIGN:
+            return "="
+        case TokenKind.OPEN_PAREN:
+            return "("
+        case TokenKind.CLOSE_PAREN:
+            return ")"
+        case TokenKind.OPEN_CURLY:
+            return "{"
+        case TokenKind.CLOSE_CURLY:
+            return "}"
+        case TokenKind.OPEN_BRACK:
+            return "["
+        case TokenKind.CLOSE_BRACK:
+            return "]"
+        case TokenKind.SEMICOLON:
+            return ";"
+        case _:
+            return "unmatched symbol"
+
+class Token:
+    def __init__(self, tokenKind: TokenKind, value: str = "", index = -1):
+        self.kind = tokenKind
+        self.value = value
+        self.index = index
+    
+    def is_a(self, *options):
+        return self.kind in options
+    
+    def debug(self):
+        print(f"{self.kind.Name()} ({self.value})")
+
+def newToken(kind: TokenKind, value: str):
+    return Token(kind, value)
---
+++ lexer.py
@@ -0,0 +1,202 @@
+from Token import *
+from Token import TokenKind as TK
+
+EOF: str = "\0"
+
+class Lexer:
+    def __init__(self, src:str, pos:int = 0, tokens:list = []):
+        self.source = src
+        self.pos = pos
+        self.tokens = tokens
+    def current(self) -> str:
+        if self.pos >= len(self.source):
+            return "\0"
+        return self.source[self.pos]
+    
+    def printTokens(self, printSource: bool = False):
+        if printSource:
+            print(self.source, end = "\n\n")
+        for token in self.tokens:
+            if token != None:
+                token.debug()
+            else:
+                print(None)
+    
+    def add(self, token: Token):
+        if isinstance(token, Token):
+            self.tokens.append(token)
+        else:
+            tokenKind: TokenKind = token # for better readability
+            self.tokens.append(Token(tokenKind, symbolize(tokenKind), self.pos))
+    
+    def advance(self):
+        self.pos += 1
+
+    def peek(self, offset: int = 1):
+        if self.pos + offset >= len(self.source):
+            return EOF
+        return self.source[self.pos + offset]
+    
+    def peek_str(self, amount: int):
+        if self.pos + amount >= len(self.source):
+            return EOF
+        return self.source[self.pos + 1:self.pos + 1 + amount]
+    
+    def tokenize_number(self) -> Token:
+        start = self.pos
+        while self.current().isdigit():
+            self.advance()
+            if self.current() == "." and self.peek().isdigit():
+                self.advance()
+                while self.current().isdigit():
+                    self.advance()
+                value = self.source[start:self.pos]
+                return Token(TK.DECIMAL, value, start)
+        value = self.source[start:self.pos]
+        return Token(TK.INTEGER, value, start)
+    
+    def tokenize_string(self) -> Token:
+        start = self.pos
+        self.pos += 1
+        while self.current() != "\"":
+            self.advance()
+        self.advance()
+        value = self.source[start:self.pos]
+        return Token(TK.STRING, value, start)
+    
+    def tokenize_alpha(self):
+        start = self.pos
+        while self.current().isalpha() or self.current() == "_":
+            self.advance()
+        Value = self.source[start:self.pos]
+        
+        def lookup_alpha(alpha):
+            match alpha:
+                case "c" if self.peek() == "\"":
+                    return TK.COMPOSITE_STR
+                case "int" | "ᛁᚾᛏ":
+                    return TK.INT
+                case "str" | "ᛋᛏᚱ" | "ᛥᚱ":
+                    return TK.STR
+                case "float" | "ᚠᛚᚩᛏ":
+                    return TK.FLOAT
+                case "bool" | "ᛒᚣᛚ":
+                    return TK.BOOL
+                case "char" | "ᚳᚻᚪᚱ":
+                    return TK.CHAR
+                case "any" | "ᛖᚾᛁ":
+                    return TK.ANY
+                case "nil" | "ᚾᛁᛚ":
+                    return TK.NIL
+                case "true" | "ᛏᚱᚣ":
+                    return TK.TRUE
+                case "false" | "ᚠᛟᛚᛋ":
+                    return TK.FALSE
+                case "and" | "ᚫᚾᛞ":
+                    return TK.AND
+                case "or" | "ᛟᚱ":
+                    return TK.OR
+                case "xor" | "ᛉᛟᚱ":
+                    return TK.XOR
+                case "nor" | "ᚾᛟᚱ":
+                    return TK.NOR
+                case "nand" | "ᚾᚫᚾᛞ":
+                    return TK.NAND
+                case _:
+                    return TK.IDENTIFIER
+
+        return Token(lookup_alpha(Value), Value, index = start)
+    
+    def Tokenize(self): # Where the magic happens B)
+        while self.current() != EOF:
+            ch = self.current()
+            if ch.isspace():
+                self.advance()
+                continue # Ignore dat shee
+            elif ch.isdigit():
+                self.add(self.tokenize_number())
+            elif ch.isalpha():
+                self.add(self.tokenize_alpha())
+            else:
+                match ch:
+                    case "+":
+                        self.add(TK.PLUS)
+                    case "-":
+                        self.add(TK.MINUS)
+                    case "*":
+                        if self.peek() == "*":
+                            self.add(TK.EXPONENT)
+                            self.advance()
+                        else:
+                            self.add(TK.ASTERISK)
+                    case "^":
+                        self.add(TK.EXPONENT)
+                    case "/":
+                        if self.peek() == "/":
+                            self.add(TK.DOUBLE_SLASH)
+                            self.advance()
+                        else:
+                            self.add(TK.SLASH)
+                    case "\"":
+                        self.add(self.tokenize_string())
+                    case "!":
+                        if self.peek() == "=":
+                            self.advance()
+                            self.add(TK.NOT_EQUAL)
+                        else:
+                            self.add(TK.DEBUG_UNKNOWN)
+                    case "?":
+                        self.add(TK.QUESTION_MARK)
+                    case ".":
+                        self.add(TK.DOT)
+                    case "=":
+                        if self.peek() == "=":
+                            self.add(TK.EQUALS)
+                            self.advance()
+                        else:
+                            self.add(TK.ASSIGN)
+                    case ";":
+                        self.add(TK.SEMICOLON)
+                    case "(":
+                        self.add(TK.OPEN_PAREN)
+                    case ")":
+                        self.add(TK.CLOSE_PAREN)
+                    case "[":
+                        self.add(TK.OPEN_BRACK)
+                    case "]":
+                        self.add(TK.CLOSE_BRACK)
+                    case "{":
+                        self.add(TK.OPEN_CURLY)
+                    case "}":
+                        self.add(TK.CLOSE_CURLY)
+                    case "%":
+                        self.add(TK.MODULO)
+                    case "≠":
+                        self.add(TK.NOT_EQUAL)
+                    case ">":
+                        if self.peek() == "=":
+                            self.add(TK.GREATER_EQUAL)
+                        else:
+                            self.add(TK.GREATER_THAN)
+                    case "<":
+                        if self.peek() == "=":
+                            self.add(TK.LESS_EQUAL)
+                        else:
+                            self.add(TK.LESS_THAN)
+                    case "≥":
+                        self.add(TK.GREATER_EQUAL)
+                    case "≤":
+                        self.add(TK.LESS_EQUAL)
+                    case "":
+                        pass
+                    case "":
+                        pass
+                    case _:
+                        self.add(TK.DEBUG_UNKNOWN)
+                self.advance()
+        self.add(TK.EOF_KIND)
+with open("./samples/sample.ᚦ", encoding="utf8") as file:
+    lexer = Lexer(file.read())
+    lexer.Tokenize()
+
+    lexer.printTokens(printSource=True)
